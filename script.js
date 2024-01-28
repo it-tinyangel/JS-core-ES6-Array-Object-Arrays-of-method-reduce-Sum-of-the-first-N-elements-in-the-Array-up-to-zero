c92a04f4664d116ac052357e811da8d4b7aebcda
Iryna Romashko <137172921+it-tinyangel@users.noreply.github.com>
2024-01-28 23:02:38 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let arr1 = [1, 2, 5, 0, 4, 5, 6];
+
+const sumBeforeZero = arr1.reduce((acc, curr) => {
+	if (curr === 0) {
+		return acc;
+	}
+	return acc + curr;
+}, 0);
+
+console.log('The sum of the first N elements up to the first zero:', sumBeforeZero);
